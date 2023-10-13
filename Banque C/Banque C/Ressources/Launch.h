4ee8ddb8bc3f0815cd5dcc37267017c6c7c05f2a
Fvbio <116072267+Fvbio@users.noreply.github.com>
2023-10-13 09:35:57 +0200

    Add files via upload

---
+++ Banque C/Banque C/Ressources/Launch.h
@@ -0,0 +1,19 @@
+//
+//  Launch.h
+//  Banque C
+//
+//  Created by Fabio Tavares Marques  on 11/10/2023.
+//
+
+#ifndef Launch_h
+#define Launch_h
+
+#include <stdio.h>
+
+int Start(void);
+void End(void);
+void Menu(void);
+int CompteValide(int);
+
+
+#endif /* Launch_h */
---
+++ Banque C/Banque C/Ressources/compte_init.h
@@ -0,0 +1,19 @@
+//
+//  compte_init.h
+//  Banque HSBC C
+//
+//  Created by Fabio Tavares Marques  on 11/10/2023.
+//
+
+#ifndef compte_init_h
+#define compte_init_h
+
+
+
+void accountInit(void);
+int validationCompte(int numero);
+
+
+
+
+#endif /* account_init_h */
